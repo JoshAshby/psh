a4c297a47528c7a4c4101ad03f8434f325d12faf
JoshAshby <joshuaashby@joshashby.com>
2013-12-23 18:30:46 -0700

    working on new admin panel
    remove hipache new controller and merge into index

--- app/controllers/admin/hipache/index.py
+++ app/controllers/admin/hipache/index.py
@@ -13,6 +13,8 @@ from seshat.route import autoRoute
 from seshat.MixedObject import MixedObject
 from seshat.objectMods import login
 
+from seshat.actions import Redirect
+
 from models.redis.hipache import hipacheModel as him
 
 from utils.paginate import Paginate
@@ -33,3 +35,24 @@ class index(MixedObject):
         self.view.data = {"page": page}
 
         return self.view
+
+    def POST(self):
+        source = self.request.getParam("source")
+        route = him.Route(source)
+
+        if route.name:
+            self.request.session.push_alert("A route entry for that already exists, please edit it, or remove it before trying to make a new route with the same source.")
+            return Redirect("/admin/hipache/"+source)
+
+        domains = self.request.getParam("destination")
+
+        name = ":".join([self.request.session.username, source])
+
+        if type(domains) is not list:
+            domains = [domains]
+
+        domains = [ domain for domain in domains if domain ]
+        domains.insert(0, name)
+        route.add(domains)
+
+        return Redirect("/admin/hipache/"+source)
